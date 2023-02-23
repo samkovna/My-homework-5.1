c287dc225d8f136a765e8e8c7a7ddcc87091dd5e
A. Stefanovich <stefanovich.anastasiya@yandex.ru>
2023-02-23 16:21:36 +0300

    add parametrizedTest

--- src/test/java/ScheduleServiceTest.java
+++ src/test/java/ScheduleServiceTest.java
@@ -1,12 +1,17 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class ScheduleServiceTest {
-    @Test
-    public void restMonth() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/schedule.csv")
+    public void restMonth(int expected, int income, int expenses, int threshold) {
         ScheduleService service = new ScheduleService();
-        int expected = 2;
-        int actual = service.calculate(100_000, 60_000, 150_000);
+
+        // int expected = 2;
+        int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
 
